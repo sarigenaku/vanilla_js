57afdec7e59388184105097bc97dc3c25c0c6870
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-06 14:31:08 +0900

    Practiced 'var': initialize, assign new value, and local scope

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 alert("hello! 하이");
 console.log("debugging 디버깅 중")
+
+// var 연습
+function abc() {
+    var x = 34
+}
+
+if (1 == 1) {
+    var x = 100
+}
+
+var y = 77
+abc()
+
+console.log(x)
+console.log(y)
