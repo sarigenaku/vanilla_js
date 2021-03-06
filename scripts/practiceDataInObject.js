c6b176c834fd9e1a004179a9281e7882059b56ed
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-06 14:52:47 +0900

    Practiced data in object

---
+++ scripts/practiceDataInObject.js
@@ -0,0 +1,14 @@
+const ReiInfo = {
+    familyName: "Ayamani",
+    age: 14,
+    favoriteColors: ["Blue", "Red", "White"], //Array in Object
+    movie: { //Object in Object
+        year: 1997,
+        title: "air"
+    },
+}
+
+console.log(ReiInfo)
+console.log(ReiInfo.familyName)
+console.log(ReiInfo.favoriteColors[0])
+console.log(ReiInfo.movie.title)
