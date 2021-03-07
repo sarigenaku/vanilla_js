1e434ebca33d6f13face2734872543eccd2dc7db
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-07 12:46:34 +0900

    Practiced event handling with if/else (after feedbacked)

--- scripts/10_eventHandlingWithIfElse.js
+++ scripts/10_eventHandlingWithIfElse.js
@@ -1,32 +1,34 @@
+const title = document.querySelector(".text5")
+
 const BASE_COLOR = "purple"
 const NEW_COLOR = "lightgreen"
-const TITLE = document.querySelector(".text5")
 
 function changeColor() {
-    if (TITLE.style.color == BASE_COLOR) {
-        TITLE.style.color = NEW_COLOR
+    const currentColor = title.style.color
+
+    if (currentColor === BASE_COLOR) {
+        title.style.color = NEW_COLOR //currentColor = NEW_COLOR는 쓰지 못한다. currentColor의 선언이 const이기 때문에.
         console.log("changed to lightgreen")
     } else {
-        TITLE.style.color = BASE_COLOR
+        title.style.color = BASE_COLOR //currentColor = BASE_COLOR는 쓰지 못한다. currentColor의 선언이 const이기 때문에.  //Q) 왜 this.style.color = BASE_COLOR로 해도 정상 작동하는 거지?
         console.log("changed to purple again")
     }
 }
 
 function init() {
-    TITLE.style.color = BASE_COLOR
-    TITLE.addEventListener("click", changeColor) //changeColor()로 할 경우, 처음부터 lightgreen이 적용되어 있음.
+    title.style.color = BASE_COLOR
+    title.addEventListener("mouseenter", changeColor) //changeColor()로 할 경우, 처음부터 lightgreen이 적용되어 있음.
 }
 
 init()
 
-function online() {
+function handleOnline() {
     console.log("Now Online!")
-
 }
 
-function offline() {
+function handleOffline() {
     console.log("Now Offline!")
 }
 
-window.addEventListener("online", online)
-window.addEventListener("offline", offline)
+window.addEventListener("online", handleOnline)
+window.addEventListener("offline", handleOffline)
