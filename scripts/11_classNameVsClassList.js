b7e47e66128b96d292c71de8150caafb46e8d9db
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-15 09:48:23 +0900

    Practiced className to change value of class attribute

---
+++ scripts/11_classNameVsClassList.js
@@ -0,0 +1,30 @@
+//CSS, JS, HTML 파일이 분리되어 쓰이도록 함 e.g. JS는 로직용이지, 웹사이트의 스타일용이 아니다
+const eva = document.getElementById("eva");
+
+const CLICKED_CLASS = "clicked";
+
+/*
+function handleClick() {
+    const hasClass = eva.classList.contains(CLICKED_CLASS);
+    if (hasClass) {
+        eva.classList.remove(CLICKED_CLASS);
+    } else {
+        eva.classList.add(CLICKED_CLASS);
+    }
+}
+*/
+
+function handleClick() {
+    const currentClassName = eva.className;
+    if (currentClassName !== CLICKED_CLASS) {
+        eva.className = CLICKED_CLASS;
+    } else {
+        eva.className = "";
+    }
+}
+
+function init() {
+    eva.addEventListener("click", handleClick);
+}
+
+init();
