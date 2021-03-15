f9cfd09f98c9f633bb95ee22e66ae2ea3ce266d0
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-15 19:18:56 +0900

    Practiced JS clock 1 (after feedbacked)

--- scripts/12_clock.js
+++ scripts/12_clock.js
@@ -1,11 +1,15 @@
-const tokeijaka = document.getElementById("clock");
+const clockContainer = document.querySelector(".js-clock"), clockTitle = clockContainer.querySelector("h1"); //Returns the first element that is a descendant of node that matches selectors. -> class의 속성값, element(div) 안의 element(h1) 등.
 
 function getTime() {
     const date = new Date();
-    const hour = date.getHours();
-    const minute = date.getMinutes();
-    const second = date.getSeconds();
-    tokeijaka.innerHTML = `Current time is ${hour}:${minute}:${second}`;
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerHTML = `Current time is ${hours}:${minutes}:${seconds}`;
 }
 
-tokeijaka.addEventListener("click", getTime);
+function init() {
+    getTime();
+}
+
+init();
