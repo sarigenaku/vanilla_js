c22a8f3d1fd83b401d5be5bff1ee2cfae952b2b5
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-18 08:52:23 +0900

    Practiced To Do List (after feedbacked)

---
+++ scripts/todo.js
@@ -0,0 +1,39 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "X";
+
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos() {
+    const toDos = localStorage.getItem(TODOS_LS);
+    if (toDos !== null) {
+        console.log("localStorage에 저장한 값 꺼내오는 코드는 아직 완성 X")
+    }
+}
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
