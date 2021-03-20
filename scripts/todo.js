17445f26e46ab2eba01839a3a20a48e81f406af4
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-21 07:24:29 +0900

    Practiced To Do List 2: add, save, and read (after feedbacked)

--- scripts/todo.js
+++ scripts/todo.js
@@ -3,43 +3,48 @@ const toDoForm = document.querySelector(".js-toDoForm"),
     toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const ARRAY_LS = []
+const toDos = [];
 
-function saveToDo(text) {
-    ARRAY_LS.push(text);
-    localStorage.setItem(TODOS_LS, JSON.stringify(ARRAY_LS)); //["aa","bb","cc","dd"] -> 쉼표 앞뒤로 한 칸 띄우기를 하지 않는다.
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //["aa","bb","cc","dd"] -> 쉼표 앞뒤로 한 칸 띄우기를 하지 않는다.
 }
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "X";
-
     const span = document.createElement("span");
+    const newId = toDos.length +1;
+
+    delBtn.innerText = "X";
     span.innerText = text;
 
     li.appendChild(delBtn); 
     li.appendChild(span); //const liVariable = li.appendChild(span); (X)
+    li.id = newId;
+
     toDoList.appendChild(li); //toDoList.appendChild(liVariable); (X)
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj); //toDos가 빈 배열이어서, localStorage에서 값을 불러오는 경우라 할지라도 중복이 일어나지 않는 것.
+    saveToDos();
 }
 
 function handleSubmit(event) {
     event.preventDefault();
     const currentValue = toDoInput.value;
-    saveToDo(currentValue);
     paintToDo(currentValue);
     toDoInput.value = "";
 }
 
 function loadToDos() {
-    const toDos = JSON.parse(localStorage.getItem(TODOS_LS));
-    if (toDos !== null) {
-        toDos.forEach(element => {
-            ARRAY_LS.push(element); //element.text (X)
-            paintToDo(element);
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
         });
-    } else {
-        console.log("새로운 ToDo를 추가해 주세용")
     }
 }
 
