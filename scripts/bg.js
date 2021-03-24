0390b2a0b99bc3c71d3b5bd02a784a2aff7e55c2
yudetamago <iminaimerudesukinisinaide@harahettana.co.jp>
2021-03-24 11:06:12 +0900

    Practiced image background (After feedbacked)

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,9 +1,22 @@
-const img = document.createElement("img");
 const body = document.querySelector("body");
-const imgNum = Math.floor(Math.random() * 3) +1;
 
-img.src = `images/${imgNum}.png`
-img.classList.add("bgImage");
-body.appendChild(img);
+const IMG_NUMBER = 3;
 
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.png`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
 
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+init();
